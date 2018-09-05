9a319a31fe29530ec63cb873325581eabcc5190b
Juan Andres <juanpromanzio@gmail.com>
2018-09-05 17:27:45 -0300

    Add files via upload

--- firmail.js
+++ firmail.js
@@ -29,10 +29,10 @@ function ejecutar(){
       				//formato de firma
 					document.getElementById("firma").innerHTML = "<font size='1'><table><tr><img src='https://scontent.fros2-2.fna.fbcdn.net/v/t1.0-1/p720x720/16939163_1349458735097462_4295419607028443057_n.png?_nc_cat=0&oh=7df1f7e50dfc21d84b092f223a49a1ee&oe=5BC55F2A' width='70' height='70' style='float:left'><tr><td><b>" + this.nombre +"</b>" + " / " + this.puesto +"</td></tr><tr><td>" + this.mail + " / " + this.num_cel + 
 	      				"</td></tr><tr><td><a href='https://www.arbusta.net' target='_blank'>Arbusta</a> / <b>"+ this.loc +"</b></td></tr>" +
-	      				"<tr><td><a href='https://es-la.facebook.com/arbustait/' target= '_blank'><img src='./icons/facebook.png'  width='13' height='13'></a>" +
-	      				"<a href='https://twitter.com/arbustait' target='_blank'><img src='./icons/twitter.png'  width='13' height='13'</a>" +
-	      				"<a href='https://www.linkedin.com/company/arbusta' target='_blank'><img src='./icons/linkedin.png' width='13' height='13'></a>"+
-	      				"<a href='https://www.instagram.com/arbustait/' target='_blank'><img src='./icons/instagram.png' width='13' height='13'></a></td></tr></tr></table></font>";
+	      				"<tr><td><a href='https://es-la.facebook.com/arbustait/' target= '_blank'><img src='https://cdn.icon-icons.com/icons2/285/PNG/256/social_facebook_box_blue_256_30649.png'  width='13' height='13'></a>" +
+	      				"<a href='https://twitter.com/arbustait' target='_blank'><img src='https://cdn.icon-icons.com/icons2/122/PNG/512/twitter_socialnetwork_20007.png'  width='13' height='13'</a>" +
+	      				"<a href='https://www.linkedin.com/company/arbusta' target='_blank'><img src='https://cdn.icon-icons.com/icons2/99/PNG/512/linkedin_socialnetwork_17441.png' width='13' height='13'></a>"+
+	      				"<a href='https://www.instagram.com/arbustait/' target='_blank'><img src='https://cdn.icon-icons.com/icons2/195/PNG/256/Instagram_23558.png' width='13' height='13'></a></td></tr></tr></table></font>";
       				}
 				};
 			}
